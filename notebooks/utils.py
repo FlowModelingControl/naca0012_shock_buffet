2b47902b3b3de1c734e5d97c1697dcb27310cb9a
Andre Weiner <andre.weiner@yahoo.de>
2021-12-23 09:41:07 +0100

    Update NACA0012 notebooks for AIAA conference; add OAT15A data.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -44,18 +44,18 @@ def fetch_force_coefficients(path):
     return np.split(pd.concat(df_list)[["t", "Cd", "Cl"]].values, 3, 1)
 
 
-def fetch_surface_data(path):
+def fetch_surface_data(path, remove_te=False):
     """Load and process surface sample data.
 
     The following processing is done:
     - split into lower and upper side
     - normalization of x with chord length
-    - removel of trailing edge data
     - sorting of all fields according to x_*
 
     Parameters
     ----------
     path - str: path to csv file
+    remove_te - bool: trailing edge data are removed if True
 
     Returns
     -------
@@ -67,16 +67,17 @@ def fetch_surface_data(path):
     data = pd.read_csv(path, sep=" ", skiprows=[
                        0, 1], header=None, names=["x", "y", "z", "f"])
     x_max = data.x.max()
-    x_up = data[(data.y >= 0) & (data.x < 0.999*x_max)].x
+    limit = 0.999 if remove_te else 1.1
+    x_up = data[(data.y >= 0) & (data.x < limit*x_max)].x
     x_up = x_up.values / x_max
-    z_up = data[(data.y >= 0) & (data.x < 0.999*x_max)].z
+    z_up = data[(data.y >= 0) & (data.x < limit*x_max)].z
     z_up = z_up.values / x_max
-    f_up = data[(data.y >= 0) & (data.x < 0.999*x_max)].f.values
-    x_low = data[(data.y < 0) & (data.x < 0.999*x_max)].x
+    f_up = data[(data.y >= 0) & (data.x < limit*x_max)].f.values
+    x_low = data[(data.y < 0) & (data.x < limit*x_max)].x
     x_low = x_low.values / x_max
-    z_low = data[(data.y < 0) & (data.x < 0.999*x_max)].z
+    z_low = data[(data.y < 0) & (data.x < limit*x_max)].z
     z_low = z_low.values / x_max
-    f_low = data[(data.y < 0) & (data.x < 0.999*x_max)].f.values
+    f_low = data[(data.y < 0) & (data.x < limit*x_max)].f.values
     ind_up = np.argsort(x_up)
     ind_low = np.argsort(x_low)
     return x_up[ind_up], z_up[ind_up], f_up[ind_up], x_low[ind_low], z_low[ind_low], f_low[ind_low]
@@ -205,7 +206,7 @@ def find_write_times(path):
     return times
 
 
-def average_surface_data(path, file_name, t_start=0.0, t_end=1000.0):
+def average_surface_data(path, file_name, t_start=0.0, t_end=1000.0, remove_te=False):
     """Average surface field in time and spanwise direction.
 
     The functions does the following processing steps:
@@ -223,6 +224,7 @@ def average_surface_data(path, file_name, t_start=0.0, t_end=1000.0):
     file_name - str: name of file with surface data; must be
         the same for all time folders
     t_* float: start and end time for averaging window
+    remove_te - bool: trailing edge data are removed if True
 
     Returns
     -------
@@ -247,7 +249,7 @@ def average_surface_data(path, file_name, t_start=0.0, t_end=1000.0):
     F_up[:, 0] = np.copy(spanwise_average(f_up, n_z))
     F_low[:, 0] = np.copy(spanwise_average(f_low, n_z))
     for i, t in enumerate(times[i_start+1:i_end]):
-        _, _, f_up, _, _, f_low = fetch_surface_data(path + t + "/" + file_name)
+        _, _, f_up, _, _, f_low = fetch_surface_data(path + t + "/" + file_name, remove_te)
         F_up[:, i+1] = np.copy(spanwise_average(f_up, n_z))
         F_low[:, i+1] = np.copy(spanwise_average(f_low, n_z))
     return (spanwise_average(x_up, n_z), spanwise_average(x_low, n_z),
@@ -336,6 +338,49 @@ def sample_surface_field(path, field_name, sample_x, sample_z, upper=True):
         sample[i+1] = data[field_ind][closest]
 
     return t, sample
+
+
+def compute_fft(t, probe, t_min, t_max, subtract_mean=True):
+    """Compute the Fast Fourier Transform of a probed signal.
+
+    A window in which to compute the FFT can be specified by
+    providing start and end time. The following link is very
+    helpful in understanding the NumPy convenience functions:
+    https://stackoverflow.com/questions/3694918/how-to-extract-frequency-associated-with-fft-values-in-python/27191172
+
+    Parameters
+    ----------
+    t - array: sample times; assumed to be equally spaced (const. dt)
+    probe - array: probe of which to compute the FFT
+    t_min - float: start of the time window
+    t_max - float: end of the time window
+    subtract_mean - bool: subtract the probes mean in the window before
+        computing the FFT; defaults to True
+
+    Returns
+    -------
+    freq - array: frequency bin values
+    amplitude - array: amplitudes corresponding to each frequency
+
+    """
+    # select values in time window
+    assert t_min < t_max, "Start time must be smaller than end time."
+    start_idx = np.absolute(t-t_min).argmin()
+    end_idx = np.absolute(t-t_max).argmin()
+    t_win = t[start_idx:end_idx+1]
+    p_win = probe[start_idx:end_idx+1]
+    # compute fft
+    dt = t_win[1] - t_win[0]
+    elapsed_time = t_win[-1] - t_win[0]
+    n_samples = t_win.shape[0]
+    df = 1.0 / elapsed_time  # frequency resolution
+    f_max = df * n_samples / 2.0  # highest detectable frequency
+    p_win = p_win - p_win.mean() if subtract_mean else p_win
+    amplitude = np.fft.fft(p_win)
+    freq = np.fft.fftfreq(amplitude.shape[0]) * n_samples * df
+    print(f"Selected {n_samples} samples; frequency resolution df={df:2.4f}Hz; f_max={f_max:2.2f}Hz")
+    pos_end = int(n_samples/2) if n_samples % 2 == 0 else int((n_samples+1)/2)
+    return freq[:pos_end], np.absolute(amplitude)[:pos_end]
 
 
 if __name__ == "__main__":
